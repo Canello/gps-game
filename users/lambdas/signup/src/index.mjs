2a29f3b057b155cf3edc8f550143e15afaab8a6b
Gustavo Canello <gustavobrandicanello@gmail.com>
2023-12-08 17:15:38 -0300

    feat: connected signup lambda with dynamodb

--- users/lambdas/signup/src/index.mjs
+++ users/lambdas/signup/src/index.mjs
@@ -1,9 +1,19 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcryptjs";
 import { validate } from "./helpers/validate.mjs";
+import {
+    DynamoDBClient,
+    PutItemCommand,
+    ScanCommand,
+} from "@aws-sdk/client-dynamodb";
+
+const client = new DynamoDBClient({ region: "sa-east-1" });
 
 export const handler = async (event) => {
     try {
         const { name, email, password } = event;
 
+        // Validate inputs
         const errors = validate(name, email, password);
         if (errors.length > 0) {
             return {
@@ -14,9 +24,46 @@ export const handler = async (event) => {
             };
         }
 
+        // Check if email is already in use
+        const scanInput = {
+            TableName: "loc-users",
+            ScanFilter: {
+                Email: {
+                    AttributeValueList: [{ S: email }],
+                    ComparisonOperator: "EQ",
+                },
+            },
+        };
+        const scanCommand = new ScanCommand(scanInput);
+        const scanResult = await client.send(scanCommand);
+        if (scanResult.Count > 0)
+            return {
+                statusCode: 400,
+                body: {
+                    errors: ["Email already in use."],
+                },
+            };
+
+        // Hash password
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+
+        // Create user
+        const input = {
+            TableName: "loc-users",
+            Item: {
+                UserId: { S: uuidv4() },
+                Name: { S: name },
+                Email: { S: email },
+                Password: { S: hash },
+            },
+        };
+        const command = new PutItemCommand(input);
+        const result = await client.send(command);
+
         return {
             statusCode: 200,
-            body: { name, email, password },
+            body: result,
         };
     } catch (err) {
         console.log("ERROR");
